89ffb5c4cf408f8a089b128cb5089a6f776992b5
Umarchik <umarjonfayzullaev2004@gmail.com>
2021-12-25 17:14:24 +0500

    neww

--- main.js
+++ main.js
@@ -15,13 +15,30 @@ formElement.addEventListener("submit", event => {
 })
 
 function renderArray(array) {
+    tbodyElement.textContent = ""
     for(let item in array) {
         const newTrElement = document.createElement("tr");
+        if(data [item] ["isDone"]) {
+            newTrElement.classList.add("table-success")
+        }
+        const newTrElement = document.createElement("tr");
         const newTodoNameElement = document.createElement("td");
+        newTodoNameElement.textContent = data[item] ["name"];
         const newButtonWrapperTdElement = document.createElement("td");
         const newDoneButtonElement = document.createElement("button");
         const newRemoveButtonElement = document.createElement("button");
 
-        
+        newDoneButtonElement.classList.add('btn', 'btn-success');
+        newDoneButtonElement.textContent = "Done"
+        newRemoveButtonElement.classList.add('btn', 'btn-danger');
+        newRemoveButtonElement.textContent = "Remove"
+
+        newButtonWrapperTdElement.appendChild(newDoneButtonElement)
+        newButtonWrapperTdElement.appendChild(newRemoveButtonElement)
+
+        newTrElement.appendChild(newTodoNameElement)
+        newTrElement.appendChild(newButtonWrapperTdElement)
+
+        tbodyElement.appendChild(newTrElement)
     }
 }
