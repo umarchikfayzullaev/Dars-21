bffb04d9c5f39503a7ed207c6530ababf2892840
Umarchik <umarjonfayzullaev2004@gmail.com>
2021-12-25 17:30:30 +0500

    nw

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-const formElement = document.querySelector('.form')
-const todoNameElement = document.querySelector('.todo-name')
-const tbodyElement = document.querySelector("#tbody")
 
-const data = JSON.parse(localStorage.getItem("data")) || []
+const formElement = document.querySelector('.form');
+const todoNameInputElement = document.querySelector(".todo-name")
+const tbodyElement = document.querySelector("#tbody");
+
+const data  = JSON.parse(localStorage.getItem("data"))  || []
 
 renderArray(data)
 
 formElement.addEventListener("submit", event => {
     event.preventDefault();
-    
+
     if (todoNameInputElement.value.length > 0) {
         data.push({
             name: todoNameInputElement.value,
             isDone: false
         })
-        
+
         renderArray(data)
     }
 })
@@ -23,19 +24,19 @@ function renderArray(array) {
     localStorage.setItem("data", JSON.stringify(array))
     tbodyElement.textContent = ""
     for(let item in array) {
-        const newTrElement = document.createElement("tr");
+        const newTrElement = document.createElement('tr');
         if(data [item] ["isDone"]) {
             newTrElement.classList.add("table-success")
         }
-        const newTodoNameElement = document.createElement("td");
-        newTodoNameElement.textContent = data[item] ["name"];
-        const newButtonWrapperTdElement = document.createElement("td");
-        const newDoneButtonElement = document.createElement("button");
-        const newRemoveButtonElement = document.createElement("button");
+        const newTodoNameElement = document.createElement('td');
+        newTodoNameElement.textContent = data[item] ["name"]
+        const newButtonWrapperTdElement = document.createElement('td');
+        const newDoneButtonElement = document.createElement('button')
+        const newRemoveButtonElement = document.createElement('button')
 
-        newDoneButtonElement.classList.add('btn', 'btn-success');
+        newDoneButtonElement.classList.add('btn', 'btn-success')
         newDoneButtonElement.textContent = "Done"
-        newRemoveButtonElement.classList.add('btn', 'btn-danger');
+        newRemoveButtonElement.classList.add('btn', 'btn-danger')
         newRemoveButtonElement.textContent = "Remove"
 
         newButtonWrapperTdElement.appendChild(newDoneButtonElement)
@@ -46,4 +47,6 @@ function renderArray(array) {
 
         tbodyElement.appendChild(newTrElement)
     }
+
+    formElement.reset()
 }
